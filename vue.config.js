25539c97ca9f4678fff288f808873ec682cd761f
Aleksey <d_team-lex@bk.ru>
2023-03-27 19:24:17 +0300

    global.scss

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'Vue 3 | Connect Four Game',
+    },
+  },
+});
