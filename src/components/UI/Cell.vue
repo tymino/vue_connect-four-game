9d5f1b15e7e0b3036e2ae40a997e5aaadec8b40f
Aleksey <d_team-lex@bk.ru>
2023-04-21 17:35:41 +0300

    winner cell style

--- src/components/UI/Cell.vue
+++ src/components/UI/Cell.vue
@@ -20,22 +20,40 @@ export default {
 
 <style lang="scss" scoped>
 @mixin cellFilled($color) {
+  position: relative;
   box-shadow: inset 0px 3px var(--color-edge);
   background-color: var($color);
+}
+@mixin cellWinner {
+  &::after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    width: var(--cell-size-winner-circle);
+    height: var(--cell-size-winner-circle);
+
+    border: 4px solid var(--color-board);
+    border-radius: 50%;
+
+    transform: translate(-50%, -50%);
+  }
 }
 
 .cell {
   --cell-size: 54px;
+  --cell-size-winner-circle: calc(var(--cell-size) / 2.4);
 
   display: flex;
   justify-content: center;
   align-items: center;
   width: var(--cell-size);
   height: var(--cell-size);
-  background-color: var(--color-background);
 
-  border-radius: 50%;
+  background-color: var(--color-background);
   border: 1px solid black;
+  border-radius: 50%;
   box-shadow: inset 0px 6px var(--color-edge);
 
   cursor: pointer;
@@ -48,5 +66,14 @@ export default {
 
 .player-2 {
   @include cellFilled(--color-player-second);
+}
+
+.player-3 {
+  @include cellFilled(--color-player-first);
+  @include cellWinner;
+}
+.player-4 {
+  @include cellFilled(--color-player-second);
+  @include cellWinner;
 }
 </style>
