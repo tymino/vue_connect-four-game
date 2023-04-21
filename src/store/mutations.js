88c8696207e16974089e43975675d70fe6e77166
Aleksey <d_team-lex@bk.ru>
2023-04-21 15:24:53 +0300

    game loop & winner

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -34,44 +34,40 @@ export const checkWinner = (state) => {
     LBRT - left-bottom right-top
   */
 
-  for (let row = 0; row < state.gameGrid.length; row++) {
-    for (let col = 0; col < state.gameGrid[row].length; col++) {
-      const isHorizontalLine =
-        state.gameGrid[row][col] === state.currentStep &&
-        state.gameGrid[row][col + 1] === state.currentStep &&
-        state.gameGrid[row][col + 2] === state.currentStep &&
-        state.gameGrid[row][col + 3] === state.currentStep;
-      const isDiagonalLTRBLine =
-        state.gameGrid[row][col] === state.currentStep &&
-        state.gameGrid[row][col + 1] === state.currentStep &&
-        state.gameGrid[row][col + 1] === state.currentStep &&
-        state.gameGrid[row][col + 1] === state.currentStep;
-      const isDiagonalLBRTLine =
-        state.gameGrid[row][col] === state.currentStep &&
-        state.gameGrid[row][col + 1] === state.currentStep &&
-        state.gameGrid[row][col + 2] === state.currentStep &&
-        state.gameGrid[row][col + 3] === state.currentStep;
-      const isVerticalLine =
-        state.gameGrid[row][col] === state.currentStep &&
-        state.gameGrid[row][col + 1] === state.currentStep &&
-        state.gameGrid[row][col + 2] === state.currentStep &&
-        state.gameGrid[row][col + 3] === state.currentStep;
+  const lineGameGrid = state.gameGrid.flat();
 
-      console.log('test', state.gameGrid[6]);
+  lineGameGrid.forEach((_, index, array) => {
+    const isHorizontalLine =
+      array[index] === state.currentStep &&
+      array[index + 1] === state.currentStep &&
+      array[index + 2] === state.currentStep &&
+      array[index + 3] === state.currentStep;
+    const isDiagonalLTRBLine =
+      array[index] === state.currentStep &&
+      array[index + 8] === state.currentStep &&
+      array[index + 16] === state.currentStep &&
+      array[index + 24] === state.currentStep;
+    const isDiagonalLBRTLine =
+      array[index + 3] === state.currentStep &&
+      array[index + 9] === state.currentStep &&
+      array[index + 15] === state.currentStep &&
+      array[index + 21] === state.currentStep;
+    const isVerticalLine =
+      array[index] === state.currentStep &&
+      array[index + 7] === state.currentStep &&
+      array[index + 14] === state.currentStep &&
+      array[index + 21] === state.currentStep;
 
-      // convert to 1d array and check valid result
+    const hasWinnerLine =
+      isHorizontalLine ||
+      isDiagonalLTRBLine ||
+      isDiagonalLBRTLine ||
+      isVerticalLine;
 
-      const hasWinnerLine =
-        isHorizontalLine ||
-        isDiagonalLTRBLine ||
-        isDiagonalLBRTLine ||
-        isVerticalLine;
-
-      if (hasWinnerLine) {
-        state.isGameEnd = true;
-      }
+    if (hasWinnerLine) {
+      state.isGameEnd = true;
     }
-  }
+  });
 };
 
 export const preparationForTheNextStep = (state) => {
