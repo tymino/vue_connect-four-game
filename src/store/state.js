fc8a0956f33c0fe38b0d40e66f065c1ec11958a9
Aleksey <d_team-lex@bk.ru>
2023-04-22 20:16:38 +0300

    update store data

--- src/store/state.js
+++ src/store/state.js
@@ -21,6 +21,24 @@ const state = {
     [0, 0, 0, 0, 4, 0, 0],
   ],
   isAiOn: false,
+  rules: {
+    title: 'rules',
+    section1: {
+      header: 'objective',
+      body: [
+        'be the first player to connect 4 of the same colored discs in a row (either vertically, horizontally, or diagonally).',
+      ],
+    },
+    section2: {
+      header: 'how to play',
+      body: [
+        'Red goes first in the game',
+        'Players must alternate turns, and only one disc can be dropped in each turn.',
+        'The game ends when there is a 4-in-a-row or a stalemate.',
+        'The starter of the previous game goes second on the next game.',
+      ],
+    },
+  },
 };
 
 export default state;
