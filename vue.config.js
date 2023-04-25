b3eef9ce45a4fdb56c9ea918c5e0bf0bd32fd798
Aleksey <d_team-lex@bk.ru>
2023-04-25 18:26:08 +0300

    update favicon

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   pages: {
     index: {
       entry: 'src/main.js',
-      title: 'Vue 3 | Connect Four Game',
+      title: 'Connect Four Game | Vue 3',
     },
   },
 });
